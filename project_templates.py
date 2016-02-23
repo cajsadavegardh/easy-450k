b522083ba4ade3661216ce77cf1d31e9809e2df8
dunordavind@gmail.com <the123to098!>
2016-02-24 00:27:44 +0100

    Added default empty files to template

--- project_templates.py
+++ project_templates.py
@@ -3,22 +3,31 @@ import argparse
 import os
 import subprocess as sp
 import yaml
+import shutil
 
 template_extension = ".tmpl"
 scripts_dir_name = "Scripts"
 directory_template_line = '[directories]'
 
-def creating_directory_message(directory_name):
-    # define colors for fancy printing
-    blue_col = '\033[94m'
-    green_col = '\033[92m'
-    fail_col = '\033[91m'
-    endc_col = '\033[0m'
-    bold_col = '\033[1m'
-    underline_col = '\033[4m'
+# define colors for fancy printing
+blue_col = '\033[94m'
+green_col = '\033[92m'
+fail_col = '\033[91m'
+endc_col = '\033[0m'
+bold_col = '\033[1m'
+underline_col = '\033[4m'
 
+
+def creating_directory_message(directory_name):
     return green_col + "[Creating project directory]: " + blue_col + directory_name + endc_col
 
+
+def creating_file_message(file_name):
+    return green_col + "[Creating file]: " + blue_col + file_name + endc_col
+
+
+def section_message(section_name):
+    return fail_col + section_name + endc_col
 
 # setup command line arguments
 parser = argparse.ArgumentParser(description='Initiate a new analysis project')
@@ -47,6 +56,12 @@ if not os.path.exists(template_path) or os.path.isdir(template_path):
 # now create target dir
 os.mkdir(project_dir)
 
+print()
+print(section_message("Creating project!"))
+print()
+print(section_message("Forming directories:"))
+
+
 # go through all the files and create a subdir for each of them
 yaml_template = yaml.load(open(template_path))
 for directory in yaml_template['directories']:
@@ -54,10 +69,24 @@ for directory in yaml_template['directories']:
     print(creating_directory_message(os.path.join(project_name, directory)))
     os.mkdir(template_dir_path)
 
+print()
+print(section_message("Creating files:"))
+
+for file_dict in yaml_template['files']:
+    source = os.path.join(program_dir, file_dict['from'])
+    destination = os.path.join(project_dir, file_dict['to'])
+    print(creating_file_message(destination))
+    shutil.copyfile(source, destination)
+
+print()
+
 # initialize git repo if scripts dir exists
 scripts_dir_path = os.path.join(project_dir, scripts_dir_name)
 if os.path.isdir(scripts_dir_path):
     os.chdir(scripts_dir_path)
     sp.call(["git", "init"])
+
+print()
+
 
 
